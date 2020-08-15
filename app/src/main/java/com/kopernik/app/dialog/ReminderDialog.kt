cbc51521a50ee2d367b58a94fa415438df660897
zhanglichao <895647043@qq.com>
2020-08-15 22:02:39 +0800

    首页

--- app/src/main/java/com/kopernik/app/dialog/ReminderDialog.kt
+++ app/src/main/java/com/kopernik/app/dialog/ReminderDialog.kt
@@ -15,10 +15,10 @@ import com.kopernik.R
 class ReminderDialog(private val context: Context) {
     private var dialog: Dialog? = null
     private var container: ConstraintLayout? = null
-    private var titleTv: TextView? = null
+
     private var msgTv: TextView? = null
     private var mBtn: Button? = null
-    private var mIconClose: ImageView? = null
+    private var tvTiming:TextView?=null
     private val display: Display
     private var showTitle = false
     private var showMsg = false
@@ -29,14 +29,12 @@ class ReminderDialog(private val context: Context) {
 
         // 获取自定义Dialog布局中的控件
         container = view.findViewById(R.id.container)
-        titleTv = view.findViewById(R.id.txt_title)
-        titleTv?.setVisibility(View.GONE)
+        tvTiming = view.findViewById(R.id.tvTiming)
         msgTv = view.findViewById(R.id.txt_msg)
-        msgTv?.setVisibility(View.GONE)
-        msgTv?.setMovementMethod(ScrollingMovementMethod.getInstance())
         mBtn = view.findViewById(R.id.btn)
-        mIconClose = view.findViewById(R.id.icon_close)
-
+        mBtn?.setOnClickListener {
+            listener?.let { it.onRequest() }
+        }
         // 定义Dialog布局和参数
         dialog = Dialog(context, R.style.AlertDialogStyle)
         dialog!!.setContentView(view)
@@ -51,15 +49,7 @@ class ReminderDialog(private val context: Context) {
         return this
     }
 
-    fun setTitle(title: String): ReminderDialog {
-        showTitle = true
-        if ("" == title) {
-            titleTv!!.text = "标题"
-        } else {
-            titleTv!!.text = title
-        }
-        return this
-    }
+
 
     fun setMsg(msg: String): ReminderDialog {
         showMsg = true
@@ -76,41 +66,18 @@ class ReminderDialog(private val context: Context) {
         return this
     }
 
-    fun setButton(
-        text: String?,
-        listener: View.OnClickListener?
-    ): ReminderDialog {
-        if (text != null && !text.isEmpty()) {
-            mBtn!!.text = text
-        } else {
-            mBtn!!.text = ""
-        }
-        mBtn!!.setOnClickListener { v ->
-            listener?.onClick(v)
-            dialog!!.dismiss()
-        }
-        mIconClose?.setOnClickListener {   dialog!!.dismiss() }
-        return this
+    private var listener:RequestListener?=null
+    open fun setOnRequestListener(requestListener: RequestListener){
+        listener=requestListener
     }
-
-    private fun setLayout() {
-        if (!showTitle && !showMsg) {
-            titleTv!!.text = "提示"
-            titleTv!!.visibility = View.VISIBLE
-        }
-        if (showTitle) {
-            titleTv!!.visibility = View.VISIBLE
-        }
-        if (showMsg) {
-            msgTv!!.visibility = View.VISIBLE
-        }
+    interface RequestListener {
+        fun onRequest()
     }
 
     val isShowing: Boolean
         get() = dialog!!.isShowing
 
     fun show() {
-        setLayout()
         dialog!!.show()
     }
 
--- app/src/main/java/com/kopernik/ui/home/fragment/HomeFragment.kt
+++ app/src/main/java/com/kopernik/ui/home/fragment/HomeFragment.kt
@@ -2,7 +2,7 @@ package com.kopernik.ui.home.fragment
 
 import android.os.Build
 import android.os.Bundle
-import android.view.LayoutInflater
+import android.view.View
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.databinding.ViewDataBinding
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.aleyn.mvvm.base.NoViewModel
 import com.kopernik.R
 import com.kopernik.app.base.NewBaseFragment
+import com.kopernik.app.dialog.ReminderDialog
 import com.kopernik.ui.home.adadpter.AutoPollAdapter
 import kotlinx.android.synthetic.main.fragment_home.*
 
@@ -38,28 +39,49 @@ open class HomeFragment: NewBaseFragment<NoViewModel, ViewDataBinding>() {
     override fun initView(savedInstanceState: Bundle?) {
         noticeRecyclerView.layoutManager=LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
 
-        var  list= arrayListOf("爱的发的更多更多故事的感受到","sdagsegdgdsgsdgsdg","asgsgagagaasfsafasa")
+        var  list1= arrayListOf("爱的发的更多更多故事的感受到","sdagsegdgdsgsdgsdg","asgsgagagaasfsafasa")
         val autoPollAdapter = activity?.let {
             AutoPollAdapter(
                 it,
-                list
+                list1
                 )
         }
         noticeRecyclerView.setAdapter(autoPollAdapter)
         //启动滚动
         noticeRecyclerView.start()
-        vfNoticeScroll.setInAnimation(context, R.anim.notice_in)
-        vfNoticeScroll.setOutAnimation(context, R.anim.notice_out)
-        val ionfo= arrayListOf("年后阿斯达啊ad阿达")
-        for (i in  ionfo) {
-            val view=LayoutInflater.from(activity).inflate(R.layout.item_notice, null)
-            val titleTv: TextView = view.findViewById(R.id.tv_title) as TextView
-            titleTv?.text = i
+    var  list= arrayListOf("10月1日起，达到Cha.in节点福利门槛用户的额外福利为节点所得用户投资","下个会早点哦","你没找吧")
+        for (i in list) {
+            val view: View = layoutInflater.inflate(R.layout.item_notice, null)
+            var content=view.findViewById<TextView>(R.id.content)
+            content.text=i
             vfNoticeScroll.addView(view)
 
+            /*view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.d("Guanggao", "点击了" + i);
+                }
+            });*/
         }
-        vfNoticeScroll.startFlipping()
+        vfNoticeScroll.setFlipInterval(2000);
+        vfNoticeScroll.startFlipping();
+        /* if(view_flipper.getDisplayedChild()==0){
+             view_flipper.getCurrentView().setOnClickListener(new View.OnClickListener() {
+                 @Override
+                 public void onClick(View arg0) {
+                     Log.d("Guanggao", "点击了");
+                 }
+             });*/
+        //悬浮按钮
+        ivFloat.setOnClickListener {
+            activity?.let { it1 ->
+                ReminderDialog(it1)
+                    .setCancelable(true)
+                    .setMsg("1")
+                    .show()
+            }
 
+        }
     }
 
 
