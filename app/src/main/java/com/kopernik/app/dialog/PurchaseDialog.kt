db1fb16f80e5a28782c39e5227548935edee42c7
zhanglichao <895647043@qq.com>
2020-08-18 17:29:29 +0800

    购买矿机器

---
+++ app/src/main/java/com/kopernik/app/dialog/PurchaseDialog.kt
@@ -0,0 +1,148 @@
+package com.kopernik.app.dialog
+
+import android.app.Dialog
+import android.os.Build
+import android.os.Bundle
+import android.os.Parcelable
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.Gravity
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import android.widget.*
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.DialogFragment
+import com.kopernik.R
+import com.kopernik.ui.asset.entity.*
+import com.kopernik.ui.asset.util.OnClickFastListener
+import com.kopernik.app.utils.KeyboardUtils
+
+class PurchaseDialog : DialogFragment(),
+    FingerprintDialog.AuthenticationCallback {
+
+
+
+    private var desc: TextView? = null
+
+
+    private var passwordEt: EditText? = null
+    private var exchangeCounts: EditText? = null
+    private var okBtn: Button? = null
+    private var type = 0
+    private var useFingerprint = false
+    private var fingerprintDialog: FingerprintDialog? = null
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog =
+            Dialog(activity!!, R.style.BottomDialog)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.dialog_purchase)
+        dialog.setCanceledOnTouchOutside(true)
+        val window = dialog.window
+        window!!.setWindowAnimations(R.style.AnimBottom)
+        window.setBackgroundDrawableResource(R.color.transparent)
+        val lp = window.attributes
+        lp.gravity = Gravity.BOTTOM
+        lp.width = WindowManager.LayoutParams.MATCH_PARENT
+        lp.height = WindowManager.LayoutParams.WRAP_CONTENT
+        //        lp.height =  getActivity().getWindowManager().getDefaultDisplay().getHeight() * 3 / 5;
+        window.attributes = lp
+        val bundle = arguments
+        type = bundle!!.getInt("type")
+        fingerprintDialog = FingerprintDialog.newInstance(2, null)
+        fingerprintDialog?.setAuthenticationCallback(this)
+
+        initView(dialog)
+        return dialog
+    }
+
+    private fun initView(dialog: Dialog) {
+        desc = dialog.findViewById(R.id.tx_desc)
+        passwordEt = dialog.findViewById(R.id.password_et)
+        passwordEt?.addTextChangedListener(passwordWatcher)
+        exchangeCounts = dialog.findViewById(R.id.exchange_counts)
+        passwordEt?.addTextChangedListener(passwordWatcher)
+        okBtn = dialog.findViewById(R.id.ok)
+        //关闭弹窗
+        dialog.findViewById<ImageView>(R.id.icon_close).setOnClickListener {
+            dismiss()
+        }
+
+
+        okBtn?.setOnClickListener(clickFastListener)
+    }
+
+    //点击确定按钮回调到页面进行网络请求处理
+    var clickFastListener: OnClickFastListener = object : OnClickFastListener() {
+        override fun onFastClick(v: View) {
+            if (useFingerprint) {
+                fingerprintDialog!!.show(fragmentManager!!, "fingerprint")
+                return
+            }
+            KeyboardUtils.hideSoftKeyboard(passwordEt)
+            listener?.let { it.onRequest(type,passwordEt!!.text.toString().trim()) }
+        }
+    }
+
+
+
+    var passwordWatcher: TextWatcher = object : TextWatcher {
+        override fun beforeTextChanged(
+            s: CharSequence,
+            start: Int,
+            count: Int,
+            after: Int
+        ) {
+        }
+
+        override fun onTextChanged(
+            s: CharSequence,
+            start: Int,
+            before: Int,
+            count: Int
+        ) {
+        }
+
+        override fun afterTextChanged(s: Editable) {
+            if (!useFingerprint) {
+                if (passwordEt!!.text.toString().isNotEmpty()) {
+                    enableBtn()
+                } else {
+                    disableBtn()
+                }
+            }
+        }
+    }
+
+    private fun disableBtn() {
+        okBtn!!.isEnabled = false
+    }
+
+    private fun enableBtn() {
+        okBtn!!.isEnabled = true
+    }
+
+
+    override fun onAuthenticationSucceeded(purpose: Int, value: String) {
+
+    }
+    private var listener:RequestListener?=null
+   open fun setOnRequestListener(requestListener: RequestListener){
+        listener=requestListener
+    }
+    interface RequestListener {
+        fun onRequest(type:Int,params:String)
+    }
+
+    companion object {
+        fun newInstance(type: Int): PurchaseDialog {
+            val fragment = PurchaseDialog()
+            val bundle = Bundle()
+            bundle.putInt("type", type)
+            fragment.arguments = bundle
+            return fragment
+        }
+    }
+}
--- app/src/main/java/com/kopernik/ui/mine/PurchaseMiningMachineryActivity.kt
+++ app/src/main/java/com/kopernik/ui/mine/PurchaseMiningMachineryActivity.kt
@@ -11,8 +11,15 @@ import com.aleyn.mvvm.base.NoViewModel
 import com.kopernik.BuildConfig
 import com.kopernik.R
 import com.kopernik.app.base.NewBaseActivity
+import com.kopernik.app.dialog.PurchaseDialog
+import com.kopernik.app.dialog.UDMTDialog
+import com.kopernik.ui.mine.adapter.OutTimeMiningMAdapter
 import com.kopernik.ui.mine.adapter.PurchaseMiningMAdapter
+import com.kopernik.ui.mine.adapter.RuntimeMiningMAdapter
 import kotlinx.android.synthetic.main.activity_purchase_mining_machinery.*
+import kotlinx.android.synthetic.main.activity_purchase_mining_machinery.llTab
+import kotlinx.android.synthetic.main.activity_purchase_mining_machinery.recyclerView
+import kotlinx.android.synthetic.main.activity_udmt_asset.*
 
 
 class PurchaseMiningMachineryActivity : NewBaseActivity<NoViewModel,ViewDataBinding>() {
@@ -21,13 +28,74 @@ class PurchaseMiningMachineryActivity : NewBaseActivity<NoViewModel,ViewDataBind
 
     override fun initView(savedInstanceState: Bundle?) {
         setTitle(getString(R.string.title_purchase_mm))
+        //购买矿机
+        llTab.setOnClickListener { onTabOnClick(true,false,false) }
+       //运行矿机
+        llTab1.setOnClickListener { onTabOnClick(false,true,false) }
+        //过时矿机
+        llTab2.setOnClickListener {  onTabOnClick(false,false,true)}
     }
 
 
     override fun initData() {
         var list= arrayListOf("gadgg","asfagadga","asfafas","asagagasgadgdsfdsfasfa","ssdsdad","Asdadasdasda","asfafafafaa")
-        recyclerView1.layoutManager= LinearLayoutManager(this)
+        recyclerView.layoutManager= LinearLayoutManager(this)
         var adpter= PurchaseMiningMAdapter(list)
-        recyclerView1.adapter=adpter
+        adpter.setOnItemClickListener { adapter, view, position ->
+
+                var dialog = PurchaseDialog.newInstance(1)
+            dialog!!.setOnRequestListener(object : PurchaseDialog.RequestListener {
+                    override fun onRequest(type: Int, params: String) {
+
+                    }
+                })
+            dialog!!.show(supportFragmentManager, "withdrawRecommed")
+        }
+        recyclerView.adapter=adpter
+
+
+
+        recyclerView1.layoutManager= LinearLayoutManager(this)
+        var adpter1= RuntimeMiningMAdapter(list)
+        recyclerView1.adapter=adpter1
+
+
+        recyclerView2.layoutManager= LinearLayoutManager(this)
+        var adpter2= OutTimeMiningMAdapter(list)
+        recyclerView2.adapter=adpter2
     }
+
+     //按钮点击
+    fun onTabOnClick(oneClick:Boolean,twoClick:Boolean,threeClick:Boolean){
+
+                if (oneClick){
+                    tv_purchase.setTextColor(resources.getColor(R.color.color_ffcf32))
+                    tv_purchase_line.setBackgroundColor(resources.getColor(R.color.color_ffcf32))
+                    recyclerView.visibility=View.VISIBLE
+                }else {
+                    tv_purchase.setTextColor(resources.getColor(R.color.white))
+                    tv_purchase_line.setBackgroundColor(resources.getColor(R.color.white))
+                    recyclerView.visibility=View.GONE
+                }
+             if (twoClick){
+                    tv_runtime_mining.setTextColor(resources.getColor(R.color.color_ffcf32))
+                    tv_runtime_mining_line.setBackgroundColor(resources.getColor(R.color.color_ffcf32))
+                    recyclerView1.visibility=View.VISIBLE
+                 }else {
+                    tv_runtime_mining.setTextColor(resources.getColor(R.color.white))
+                    tv_runtime_mining_line.setBackgroundColor(resources.getColor(R.color.white))
+                    recyclerView1.visibility=View.GONE
+                 }
+             if (threeClick){
+                tv_outtime_mining.setTextColor(resources.getColor(R.color.color_ffcf32))
+                tv_outtime_mining_line.setBackgroundColor(resources.getColor(R.color.color_ffcf32))
+                recyclerView2.visibility=View.VISIBLE
+            }else {
+                 tv_outtime_mining.setTextColor(resources.getColor(R.color.white))
+                 tv_outtime_mining_line.setBackgroundColor(resources.getColor(R.color.white))
+                recyclerView2.visibility=View.GONE
+            }
+
+       }
+
 }
